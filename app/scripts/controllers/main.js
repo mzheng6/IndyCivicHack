0a73accdbc508e590b123756dafadd5a8b05b2d2
mzheng6 <mzheng6@illinois.edu>
2015-06-06 17:15:54 -0400

    new changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,4 +11,30 @@ angular.module('hackforindyApp')
   .controller('MainCtrl', function ($http) {
    
    	var vm = this;
+   	var url = 'http://localhost:8000/sql/';
+
+   	vm.insert = function(formData)
+   	{
+   		var url = 'http://localhost:8000/auth/signup';
+   		$http
+		.post(url , {
+			org : formData.org, 
+			fullname : formData.fullname,
+			email : formData.email,
+			trans : formData.trans,
+			goal : formData.goal
+		})
+		.success(function(data){
+			if(!(data.message === "Error Occured")) {
+				$window.sessionStorage.token = data.token;	
+				$location.path('/profile'); // redirect (search for now, then change to profile later)
+			} else {
+				//  need to display to user that their signup failed
+				formData.password = '';
+			}
+		});
+
+
+   	}
+
   });
