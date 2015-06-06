94d38946278c1735807d7983dc1f64185312f81e
mzheng6 <mzheng6@illinois.edu>
2015-06-06 13:46:51 -0400

    michelles stuff

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackforindyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackforindyApp
+ */
+angular.module('hackforindyApp')
+  .controller('MainCtrl', function ($http) {
+   
+   	var vm = this;
+  });
